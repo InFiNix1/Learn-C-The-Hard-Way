56e97372c29f9c11ecb1fc827cf1c6473a2dcf4c
Sauronil Das <sauronildas10@gmail.com>
2020-05-26 16:47:31 +0530

    First Program

---
+++ Exercise-1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(int argc, char* argv[])
+{
+    puts("There is No Despair Without Hope");
+    puts("Nature Does Not Hurry, Yet Everything is Accomplished");
+    puts("It is Darkest before Dawn");
+    puts("Carry on my wayward son, There will Peace when you are gone");
+    puts("Someday We will dance with those \"Lions\"");
+    return 0;
+}
