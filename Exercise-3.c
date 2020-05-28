ca903dd2ac5e7db06a69f45dec9ac04d9a25d7c0
Sauronil Das <sauronildas10@gmail.com>
2020-05-28 18:59:23 +0530

    Formatted Printing

---
+++ Exercise-3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(int argc, char* argv[])
+{
+    int age=259829388;
+    int height=5.5;
+
+    printf("I am \v%d \vYears Old\n", age);
+    printf("I am \v%.2d \vInces Tall\n", height);
+    return 0;
+}
