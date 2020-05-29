1d0ee9afbbf5e68e3f3b99237840e1c80f56ae47
Sauronil Das <sauronildas10@gmail.com>
2020-05-29 19:17:49 +0530

    Basic Structure of a C Program

---
+++ Exercise-5.c
@@ -0,0 +1,12 @@
+/*This is a Comment*/
+#include<stdio.h>
+
+int main(int argc, char* argv[])
+{
+    int distance=100;
+
+    //This is also a comment
+    
+    printf("You are %d Miles Away from the \"Truth\"\n", distance);
+    return 0;
+}
--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 CFLAGS=-Wall -g
 
-all: Exercise-1 Exercise-3 Exercise-4
+all: Exercise-1 Exercise-3 Exercise-4 Exercise-5
 
 clean:
-	rm -f Exercise-1 Exercise-3 Exercise-4
+	rm -f Exercise-1 Exercise-3 Exercise-4 Exercise-5
 
 
